eaa4407d2d6e35a89b90f035094bed8b43cf4a09
zhonglin <zixiong.lzx@alibaba-inc.com>
2021-03-29 17:42:06 +0800

    增加旧数据利用逻辑，代码优化

--- morefun/fetch.js
+++ morefun/fetch.js
@@ -1,59 +1,71 @@
 require('isomorphic-fetch');
 
-let TIME_OUT = 100;
-let start_orderId = 30138635; // 8位数
-let count = 1000;
-let phone = '13241625824'; // 泽民
-// let phone = '17600546600'; // 雄
+const TIME_OUT = 100;
+const start_orderId = 30121000; // 8位数
+const count = 1000;
+// const phone = '13241625824'; // 泽民
+const phone = '17600546600'; // 雄
 
-const orderIdList = new Array(count).fill(0).map((o, i) => start_orderId + i);
+const oldData = require('./data'); // 旧数据
+const returnList = [];
+const returnListImportant = [];
 
 const api_coupon = (orderId) => `https://api.i-morefun.com/app/coupon/v1/couponTypeList?orderId=${orderId}&phone=${phone}`;
 const resultUrl = (orderId) => `https://static.i-morefun.com/redEnvelopes/index.html?orderId=1604394383297_p_1002&id=${orderId}`
 
+// 网络查询数据
 const getInfo = (id) => fetch(api_coupon(id))
-    .then(res => res.json())
-    .catch(e => {
-        console.log(e);
-    });
+  .then(res => res.json())
+  .catch(e => {
+    console.log(e);
+  });
 
-const returnList = [];
+// 校验结果是否符合
 const validResult = (modelData = [], strict = true) => {
-    if ((strict && modelData.length > 2 && modelData.length < 5) || (!strict && modelData.length > 0)) {
-        if (!modelData.find(({telPhone = '', couponPrice = 0}) => telPhone === "176*****606" || couponPrice === 0.88)) {
-            return true;
-        }
+  if ((strict && modelData.length > 2 && modelData.length < 5) || (!strict && modelData.length > 0)) {
+    if (!modelData.find(({telPhone = '', couponPrice = 0}) => telPhone === "176*****606" || couponPrice === 0.88)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 };
 
-// Promise.all(orderIdList.map(getInfo)).then(jsonList => {
-//     jsonList.forEach(({modelData = []}, i) => {
-//         console.log(orderIdList[i], modelData.length);
-//         if (validResult(modelData)) returnList.push(orderIdList[i])
-//     });
-//     returnList.forEach(id => {
-//         console.log(resultUrl(id));
-//     });
-// });
+// 延迟
+const wait = (time) => new Promise(resolve => {
+  setTimeout(() => resolve(), time);
+});
 
-
-const continueQuery = (orderId) => {
-    if (orderId > (start_orderId + count)) {
-        returnList.forEach(id => {
-            console.log(resultUrl(id));
-        });
-        return null;
+// 遍历查询
+const query = async (orderIdList) => {
+  for (let i = 0; i < orderIdList.length; i++) {
+    let orderId = orderIdList[i];
+    const {modelData = []} = await getInfo(orderId);
+    console.log(orderId, modelData.length);
+    if (validResult(modelData, false)) {
+      console.log(resultUrl(orderId));
+      returnList.push(orderId);
     }
-    getInfo(orderId).then(({modelData = []}) => {
-        console.log(orderId, modelData.length);
-        if (validResult(modelData, false)) console.log(resultUrl(orderId));
-        if (validResult(modelData)) {
-            console.log(resultUrl(orderId), '!!!!!!!');
-            returnList.push(orderId);
-        }
-        setTimeout(() => continueQuery(orderId + 1), TIME_OUT);
-    })
-};
+    if (validResult(modelData)) {
+      console.log(resultUrl(orderId), '!!!!!!!');
+      returnListImportant.push(orderId);
+    }
+    await wait(TIME_OUT);
+  }
+}
 
-continueQuery(start_orderId);
+// 获取数组
+const getList = (startId = 0, length = 0) => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(startId + i);
+  }
+  return arr;
+}
+
+// 最新数组由旧数据+新数据
+const orderIdList = [...oldData, ...getList(start_orderId, count)];
+query(orderIdList).then(() => {
+  console.log("===================================================")
+  returnList.forEach(id => console.log(resultUrl(id)));
+  returnListImportant.forEach(id => console.log(resultUrl(id), '!!!!!!!'));
+});
