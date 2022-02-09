6747469b7212a95629c1bb86590685346cb803cc
linzixiong <linzixiong@bytedance.com>
2022-02-09 18:40:06 +0800

    新增地图打点功能

---
+++ position/main.js
@@ -0,0 +1,15 @@
+import { locationList } from "./location.js";
+
+const map = new BMapGL.Map('container');
+map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+// 地图居中
+const point = new BMapGL.Point(116.404, 39.915);
+map.centerAndZoom(point, 15);
+
+locationList.forEach(item => {
+    const point = new BMapGL.Point(item.lng, item.lat)
+    const circle = new BMapGL.Circle(point, 500, {strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5}); //创建圆
+    map.addOverlay(circle); //增加圆
+    map.addOverlay(new BMapGL.Marker(point, {title: item}))
+})
