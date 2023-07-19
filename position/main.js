dd41cb4d931a6437f8b3c522a565e48cce0618e3
linzixiong <lzxil@gmail.com>
2023-07-20 01:17:18 +0800

    打车地理位置获取

--- position/main.js
+++ position/main.js
@@ -11,5 +11,5 @@ locationList.forEach(item => {
     const point = new BMapGL.Point(item.lng, item.lat)
     const circle = new BMapGL.Circle(point, 500, {strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5}); //创建圆
     map.addOverlay(circle); //增加圆
-    map.addOverlay(new BMapGL.Marker(point, {title: item}))
+    map.addOverlay(new BMapGL.Marker(point, {title: item.name}))
 })
