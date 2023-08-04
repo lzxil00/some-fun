b45019b3d0f8479cbafb34ea3fa2efbefb142c5b
linzixiong <linzixiong@bytedance.com>
2023-08-04 15:26:38 +0800

    自动轮询工具

---
+++ tool/autoCheck.js
@@ -0,0 +1,80 @@
+/**
+ * 填写fetch方法用于数据请求
+ * @returns {Promise<Response>}
+ */
+const fetchFunc = () => {
+    return fetch('');
+}
+
+/**
+ * 填写数据请求回来后的判断逻辑
+ * @returns {boolean}
+ */
+const checkFunc = (json) => {
+    return false
+}
+
+/**
+ * 查询间隔
+ * @type {number}
+ */
+const DELAY_TIME = 5 * 1000;
+
+/**
+ * 主程序
+ * @returns {Promise<void>}
+ */
+const main = async () => {
+    let count = 0;
+    const delay = (time) => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, time);
+        });
+    };
+
+    const playMusic = () => {
+        const music = 'https://downsc.chinaz.net/Files/DownLoad/sound1/202007/13195.mp3'; // 提示音乐
+        let audio = new Audio(music);
+        audio.muted = false;
+        audio.play();
+    }
+
+    const checkTicket = async (fetchFunc, checkFunc) => {
+        count++;
+        await delay(DELAY_TIME * 2);
+        return new Promise((resolve, reject) => {
+            fetchFunc().then(res => res.json()).then(json => {
+                if (checkFunc(json)) {
+                    const date = new Date();
+                    const dataStr = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+                    console.log(`查询第${count}次，通过检测，时间：${dataStr}`)
+                    resolve();
+                } else {
+                    console.log(`查询第${count}次，检测失败，继续查询`)
+                    reject();
+                }
+            }).catch(e => {
+                console.log(`查询第${count}次，请求失败，原因：`, e)
+            })
+        })
+    }
+
+    const autoCheck = (fetchFunc, checkFunc, globalResolve) => {
+        return new Promise(resolve => {
+            checkTicket(fetchFunc, checkFunc).then(() => {
+                globalResolve ? globalResolve() : resolve()
+            }).catch(() => {
+                return autoCheck(fetchFunc, checkFunc, globalResolve || resolve)
+            })
+        })
+    }
+
+    await delay(DELAY_TIME);
+    await autoCheck(fetchFunc, checkFunc);
+    playMusic();
+}
+
+const result = main();
+console.log(result)
